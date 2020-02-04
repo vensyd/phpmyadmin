32ad3f472c5c6dc2e96bcfdccacfc24b7c791a6e
Sreedhar <vensyd@gmail.com>
2020-02-05 08:52:41 +1000

    Added SSL option

--- config.inc.php
+++ config.inc.php
@@ -25,6 +25,7 @@ $i = 0;
 $i++;
 /* Authentication type */
 $cfg['CheckConfigurationPermissions'] = false;
+$cfg['Servers'][$i]['ssl'] = true; 
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
 $cfg['Servers'][$i]['host'] = 'appiandes.mysql.database.azure.com';
