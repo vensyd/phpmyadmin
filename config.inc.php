1df21ad3771dad2fd68e850f35eedfb943ce70eb
Sreedhar <vensyd@gmail.com>
2020-02-05 08:56:07 +1000

    Added blowfishsecret

--- config.inc.php
+++ config.inc.php
@@ -12,7 +12,7 @@ declare(strict_types=1);
  * This is needed for cookie based authentication to encrypt password in
  * cookie. Needs to be 32 chars long.
  */
-$cfg['blowfish_secret'] = ''; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
+$cfg['blowfish_secret'] = 'thisisDepartofEnvironmentSciencesMySQL2020'; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
 
 /**
  * Servers configuration
